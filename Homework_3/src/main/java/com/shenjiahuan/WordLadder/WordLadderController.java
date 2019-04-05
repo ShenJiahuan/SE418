928b6727b14dde5ccf9b974b20ba23cbd43b0a4d
shenjiahuan <shenjiahuan@sjtu.edu.cn>
2019-04-05 22:23:15 +0800

    first commit of homework 3

---
+++ Homework_3/src/main/java/com/shenjiahuan/WordLadder/WordLadderController.java
@@ -0,0 +1,34 @@
+package com.shenjiahuan.WordLadder;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WordLadderController {
+    private static final String template = "Hello";
+    private final AtomicLong counter = new AtomicLong();
+
+    public WordLadderController() {
+    }
+
+    @RequestMapping({"/wordladder"})
+    public WordLadderResult wordLadder(@RequestParam("from") String from, @RequestParam("to") String to) {
+        ArrayList<String> result = null;
+        int status = 0;
+
+        try {
+            result = (new WordLadder("/static/EnglishWords.txt")).get(from, to);
+            if (result == null) {
+                status = -1;
+            }
+        } catch (IOException | NullPointerException ex) {
+            status = -2;
+        }
+
+        return new WordLadderResult(result, status);
+    }
+}

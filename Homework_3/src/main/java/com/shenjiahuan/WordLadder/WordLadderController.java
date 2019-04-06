8ae000c5e4eba5bd7f119590f7b0977bd0d027ec
shenjiahuan <shenjiahuan@sjtu.edu.cn>
2019-04-06 13:52:06 +0800

    changed dict initialize time

--- Homework_3/src/main/java/com/shenjiahuan/WordLadder/WordLadderController.java
+++ Homework_3/src/main/java/com/shenjiahuan/WordLadder/WordLadderController.java
@@ -2,14 +2,24 @@ package com.shenjiahuan.WordLadder;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
+
 @RestController
 public class WordLadderController {
-    public WordLadderController() {
+    private WordLadder wordLadder;
+
+    @PostConstruct
+    private void init() {
+        System.out.println("Init");
+        try {
+            this.wordLadder = new WordLadder("/static/EnglishWords.txt");
+        } catch (IOException ex) {
+            this.wordLadder = null;
+        }
     }
 
     @RequestMapping({"/wordladder"})
@@ -18,11 +28,11 @@ public class WordLadderController {
         int status = 0;
 
         try {
-            result = (new WordLadder("/static/EnglishWords.txt")).get(from, to);
+            result = this.wordLadder.get(from, to);
             if (result == null) {
                 status = -1;
             }
-        } catch (IOException | NullPointerException ex) {
+        } catch (NullPointerException ex) {
             status = -2;
         }
 

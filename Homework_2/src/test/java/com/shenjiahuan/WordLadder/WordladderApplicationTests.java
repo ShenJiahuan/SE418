4083d8f038911dd938d0036e2164dacb0c3f9235
shenjiahuan <shenjiahuan@sjtu.edu.cn>
2019-04-10 17:07:48 +0800

    added test case

--- Homework_2/src/test/java/com/shenjiahuan/WordLadder/WordladderApplicationTests.java
+++ Homework_2/src/test/java/com/shenjiahuan/WordLadder/WordladderApplicationTests.java
@@ -1,17 +1,26 @@
 package com.shenjiahuan.WordLadder;
 
 import static org.hamcrest.Matchers.containsString;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.unauthenticated;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrlPattern;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.FormLoginRequestBuilder;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -32,14 +41,56 @@ public class WordladderApplicationTests {
     }
 
     @Test
+    public void loginWithValidUserThenAuthenticated() throws Exception {
+        FormLoginRequestBuilder login = formLogin()
+                .user("SE418")
+                .password("SE418");
+
+        mockMvc.perform(login)
+                .andExpect(authenticated().withUsername("SE418"));
+    }
+
+    @Test
+    public void loginWithInvalidUserThenUnauthenticated() throws Exception {
+        FormLoginRequestBuilder login = formLogin()
+                .user("invalid")
+                .password("invalidpassword");
+
+        mockMvc.perform(login)
+                .andExpect(unauthenticated());
+    }
+
+    @Test
+    public void accessSecuredResourceUnauthenticatedThenRedirectsToLogin() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrlPattern("**/login"));
+    }
+
+    @Test
+    @WithMockUser
+    public void accessSecuredResourceAuthenticatedThenOk() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithMockUser
     public void shouldHaveSuccessStatus() throws Exception {
-        this.mockMvc.perform(get("/wordladder?from=hello&to=world")).andExpect(status().isOk())
+        this.mockMvc.perform(get("/wordladders")
+                                .param("from", "hello")
+                                .param("to", "world"))
+                .andExpect(status().isOk())
                 .andExpect(content().string(containsString("\"status\":0")));
     }
 
     @Test
+    @WithMockUser
     public void shouldHaveNoLadderStatus() throws Exception {
-        this.mockMvc.perform(get("/wordladder?from=success&to=fail")).andExpect(status().isOk())
+        this.mockMvc.perform(get("/wordladders")
+                                .param("from", "successful")
+                                .param("to", "fail"))
+                .andExpect(status().isOk())
                 .andExpect(content().string(containsString("\"status\":-1")));
     }
 

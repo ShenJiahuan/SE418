0a00c8bc76418aa19861d2c5f9cbd0ce722361cb
shenjiahuan <shenjiahuan@sjtu.edu.cn>
2019-04-14 17:35:06 +0800

    changed manage port and added actuator test

---
+++ Homework_2/src/test/java/com/shenjiahuan/WordLadder/ActuatorTest.java
@@ -0,0 +1,37 @@
+package com.shenjiahuan.WordLadder;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@AutoConfigureMockMvc
+@SpringBootTest
+public class ActuatorTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testActuatorStatus() throws Exception {
+        this.mockMvc.perform(get("/actuator/health"))
+                .andExpect(status().isOk())
+                .andExpect(content().json("{'status':'UP'}"));
+    }
+
+    @Test
+    public void testActuatorInfo() throws Exception {
+        this.mockMvc.perform(get("/actuator/info"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("{}"));
+    }
+}
--- Homework_2/src/test/java/com/shenjiahuan/WordLadder/WordladderApplicationTests.java
+++ Homework_2/src/test/java/com/shenjiahuan/WordLadder/WordladderApplicationTests.java
@@ -12,15 +12,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 
 @RunWith(SpringRunner.class)
 @AutoConfigureMockMvc
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@TestPropertySource(properties = {"management.port=0"})
+@SpringBootTest
 public class WordladderApplicationTests {
 
     @Autowired

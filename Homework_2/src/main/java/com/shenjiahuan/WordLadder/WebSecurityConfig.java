1dbf0658033b18570b38cd294ff833e850c5bfb5
shenjiahuan <shenjiahuan@sjtu.edu.cn>
2019-04-14 16:59:05 +0800

    changed login logic and unauthorized response

--- Homework_2/src/main/java/com/shenjiahuan/WordLadder/WebSecurityConfig.java
+++ Homework_2/src/main/java/com/shenjiahuan/WordLadder/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.shenjiahuan.WordLadder;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -15,9 +16,22 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private MyAuthenticationSuccessHandler myAuthenticationSuccessHandler;
+
+    @Autowired
+    private MyAuthenticationFailureHandler myAuthenticationFailureHandler;
+
+    @Autowired
+    private UnauthorizedEntryPoint unauthorizedEntryPoint;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+
         http
+            .exceptionHandling().authenticationEntryPoint(unauthorizedEntryPoint)
+                .and()
             .csrf().
                 disable()
             .authorizeRequests()
@@ -26,7 +40,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
             .formLogin()
                 .loginPage("/login")
-                .permitAll();
+                .permitAll()
+                .successHandler(myAuthenticationSuccessHandler)
+                .failureHandler(myAuthenticationFailureHandler);
     }
 
     @Bean
